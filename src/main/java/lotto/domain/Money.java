98041ed550bf8fa6f38f5339bc1eb0b8f99fac34
nahyeon99 <nahyeonee99@gmail.com>
2023-12-01 23:05:43 +0900

    refactor(domain/Money): 금액 정적 변수 타입 BigDecimal로 변경

--- src/main/java/lotto/domain/Money.java
+++ src/main/java/lotto/domain/Money.java
@@ -6,9 +6,8 @@ import lotto.exception.ExceptionMessage;
 public class Money {
     public static final Money ZERO = Money.wons(0);
     private static final Money MINUS_ONE = Money.wons(-1);
-    private static final long MIN_LOTTOS_PURCHASE_AMOUNT = 1_000;
-    private static final long MAX_LOTTOS_PURCHASE_AMOUNT = 300_000;
-    private static final long LOTTO_PER_AMOUNT = 1_000;
+    private static final BigDecimal LOTTO_PER_AMOUNT = BigDecimal.valueOf(1_000);
+    private static final BigDecimal MAX_LOTTOS_PURCHASE_AMOUNT = BigDecimal.valueOf(300_000);
 
     private final BigDecimal amount;
 
@@ -20,21 +19,25 @@ public class Money {
         return new Money(BigDecimal.valueOf(amount));
     }
 
-    public static Money createMoneyByPurchaseLottos(final long amount) {
+    public static Money createMoneyByPurchaseLottos(final int amount) {
         validateLottosAmountUnit(amount);
         validateLottosAmountSize(amount);
 
         return new Money(BigDecimal.valueOf(amount));
     }
 
-    private static void validateLottosAmountSize(long amount) {
-        if (amount < MIN_LOTTOS_PURCHASE_AMOUNT || amount > MAX_LOTTOS_PURCHASE_AMOUNT) {
+    public BigDecimal calculateLottoCount() {
+        return amount.divide(LOTTO_PER_AMOUNT);
+    }
+
+    private static void validateLottosAmountSize(int amount) {
+        if (amount < LOTTO_PER_AMOUNT.intValue() || amount > MAX_LOTTOS_PURCHASE_AMOUNT.intValue()) {
             throw new IllegalStateException(ExceptionMessage.INVALID_MONEY_SIZE.getKorean());
         }
     }
 
-    private static void validateLottosAmountUnit(long amount) {
-        if (amount / LOTTO_PER_AMOUNT != 0) {
+    private static void validateLottosAmountUnit(int amount) {
+        if (amount % LOTTO_PER_AMOUNT.intValue() != 0) {
             throw new IllegalStateException(ExceptionMessage.INVALID_MONEY_UNIT.getKorean());
         }
     }
