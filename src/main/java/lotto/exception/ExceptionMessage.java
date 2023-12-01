6c4bb90c7cf0e2dbf0d94902515b8835b26c1c5b
nahyeon99 <nahyeonee99@gmail.com>
2023-12-01 23:09:01 +0900

    feat(exception/ExceptionMessage): 예외 메시지 추가

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -5,6 +5,7 @@ public enum ExceptionMessage {
     INVALID_NUMBER_FORMAT("정수로 입력해 주세요."),
     INVALID_MONEY_UNIT("구입 금액은 1,000원 단위로 입력해 주세요."),
     INVALID_MONEY_SIZE("구입 금액은 1,000원 이상 300,000원 이하로 입력해 주세요."),
+    INVALID_LOTTO_NUMBER_SIZE("로또 번호는 6개로 입력해 주세요."),
     INVALID_LOTTO_NUMBER_RANGE("로또 번호는 1~45 범위로 입력해 주세요."),
     INVALID_DUPLICATE_LOTTO_NUMBER("로또 번호는 중복 없이 입력해 주세요."),
     INVALID_DUPLICATE_BONUS_LOTTO_NUMBER("보너스 번호와 로또 번호는 중복 없이 입력해 주세요."),
